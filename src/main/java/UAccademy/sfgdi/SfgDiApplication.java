577197481bf24e4fbc6b9bb29fa975713493d0bc
FabrizioBrunoWePlus <fabrizio.bruno@we-plus.eu>
2023-02-03 15:18:10 +0100

    Start Project Dipendecy Injection

--- src/main/java/UAccademy/sfgdi/SfgDiApplication.java
+++ src/main/java/UAccademy/sfgdi/SfgDiApplication.java
@@ -1,44 +1,13 @@
 package UAccademy.sfgdi;
 
-import UAccademy.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
-
-		I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-		System.out.println(i18nController.sayHello());
-
-		MyController myController = (MyController) ctx.getBean("myController");
-		System.out.println("------------Primary Bean");
-		System.out.println(myController.sayHello());
-
-		System.out.println("------------Property");
-
-		PropertyInjectionController propertyInjectionController = (PropertyInjectionController) ctx.getBean("propertyInjectionController");
-		System.out.println(propertyInjectionController.getGreeting());
-
-		System.out.println("------------Setters");
-
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("------------Constructor");
-
-		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-		System.out.println(constructorInjectedController.getGreeting());
-
-		System.out.println("------------Another Bean without Qualifier");
-
-		AnotherInjectedController anotherInjectedController = (AnotherInjectedController) ctx.getBean("anotherInjectedController");
-		System.out.println(anotherInjectedController.getGreeting());
-
+		SpringApplication.run(SfgDiApplication.class, args);
 	}
 
 }
